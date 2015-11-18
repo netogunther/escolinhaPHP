6df4a96c1a8d5e861ae310f9a6b87efbb0c8b712
Tarcísio Nunes <github@tarcnux.com>
2015-11-18 19:39:42 -0200

    Criação do arquivo index.php

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <h1>Manuseio de Formulário com PHP</h1>
+        <h2>Referências:</h2>
+        <ul>
+            
+            <li><a href="http://www.w3schools.com/php">
+                    W3Schools</a></li>
+            
+            <li><a href="http://php.net/">
+                    Manual do PHP</a></li>
+            
+        </ul>
+        <?php
+        
+        // put your code here
+        ?>
+    </body>
+</html>
